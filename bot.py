9ee8eaa8ae2a8db6ed0a41d814b751c2c01155f4
Bernard Berdikul <b.berdicul@ylab.io>
2022-03-24 16:23:25 +0600

    Init project

---
+++ bot.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import BOT_TOKEN
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Пиши, повторю за тобой")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
