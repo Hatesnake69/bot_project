ed31cf43bd0dfce8bbe43131983e050d34b95c57
Hrabr88 <hrabr88ov@yandex.ru>
2022-03-27 18:43:00 +0300

    Подключил redis, протестировал запись и получение данных из кэша. в команду "help" и хэндлер для текста прописал сохранение текста команды в кэш, а в команду "start" и хэндлер для текста прописал вывод текста из кэша
    сам редис запускаю предворительно в докере командой
    docker run -p 6379:6379 -d redis:5

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, cache
 
 
 bot = Bot(token=BOT_TOKEN)
@@ -11,17 +11,23 @@ dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
+    data = await cache.get_data(chat=message.chat, user=message.from_user.username)
+    print(data)
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
+    await cache.set_data(chat=message.chat, user=message.from_user.username, data=message.text)
+    print(message.text)
     await message.reply("Пиши, повторю за тобой")
 
 
 @dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+async def echo_message(message: types.Message):
+    await cache.set_data(chat=message.chat, user=message.from_user.username, data=message.text)
+    print(message.text)
+    await bot.send_message(message.from_user.id, message.text)
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+
 load_dotenv()
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN")
+HOST: str = os.getenv('HOST')
+PORT: str = os.getenv('PORT')
+
+
+cache = RedisStorage2()
