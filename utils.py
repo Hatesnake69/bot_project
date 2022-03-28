0c25d9e096af58aebacfa9071c69d79f1f27d5f6
shigaevaler@gmail.com <shigaevaler@gmail.com>
2022-03-28 10:16:14 +0400

    function is_day_off/fix

--- utils.py
+++ utils.py
@@ -14,7 +14,4 @@ def is_day_off(date: datetime.date) -> bool:
 
     year, month, day = date.year, date.month, date.day
     response = requests.get(f'https://isdayoff.ru/api/getdata?year={year}&month={month}&day={day}')
-    if response.status_code == 200:
-        return True if response.text != '0' else False
-    else:
-        return False
+    return True if response.ok and response.text != '0' else False
