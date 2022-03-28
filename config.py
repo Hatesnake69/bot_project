514a613679cd27d14a4807d3edb7b05151c70886
Hrabr88 <hrabr88ov@yandex.ru>
2022-03-28 12:21:39 +0300

    Поменял название переменной для порта

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN")
 REDIS_HOST: str = os.getenv('REDIS_HOST')  # это для редиса
-PORT: str = os.getenv('PORT')  # это для редиса
+REDIS_PORT: str = os.getenv('REDIS_PORT')  # это для редиса
 
 
-cache = RedisStorage2(host=REDIS_HOST, port=int(PORT))
+cache = RedisStorage2(host=REDIS_HOST, port=int(REDIS_PORT))
