40e3c68038acd020486034ea49866996c2e970bd
icedevil777111 <icedevil777111@mail.ru>
2022-04-05 13:05:27 +0300

    add flake8, test authorization.py, common.py

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
-from aiogram import Bot
+import pandas_gbq
+from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+
+from app.handlers.authorization import register_handlers_authorization
 from app.handlers.common import register_handlers_common
 from app.handlers.create_event import register_handlers_create_event
 from app.handlers.faq import register_handlers_faq
-from app.handlers.authorization import register_handlers_authorization
-from config import BOT_TOKEN
-from config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, webhook_url
-
-
-from config import cache
-from scheduler import scheduler
+from config import (
+    BOT_TOKEN,
+    PROJECT,
+    WEBAPP_HOST,
+    WEBAPP_PORT,
+    WEBHOOK_PATH,
+    cache,
+    credentials,
+    webhook_url,
+)
+from graph import get_dataframe_for_graph, get_image, get_xlabel_for_graph
+from scheduler import TODAY, scheduler, start_sheduler
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot=bot, storage=cache)
@@ -19,6 +27,41 @@ register_handlers_common(dp)
 register_handlers_create_event(dp)
 register_handlers_faq(dp)
 register_handlers_authorization(dp)
+
+
+@dp.message_handler()
+async def echo_message(message: types.Message):
+    await cache.set_data(
+        chat=message.chat, user=message.from_user.username, data=message.text
+    )  # запись в кэш
+    await bot.send_message(message.from_user.id, message.text)
+
+
+@dp.message_handler()
+async def scheduled_all():
+    """
+    Отправляет график пользователю
+    """
+    query_users = """ SELECT DISTINCT telegram_id, fullname
+         FROM TG_Bot_Stager.salaryDetailsByTrackdate """
+    df_users = pandas_gbq.read_gbq(
+        query_users, project_id=PROJECT, credentials=credentials
+    )
+
+    for user_id in df_users.telegram_id:
+
+        caption = "График!"
+        dataframe = get_dataframe_for_graph(user_id, TODAY)
+        if not dataframe.empty:
+            labels = get_xlabel_for_graph(dataframe)
+            get_image(dataframe, labels)
+
+            await bot.send_photo(
+                user_id, open("saved_graph.png", "rb"), caption=caption
+            )
+
+
+start_sheduler(scheduled_all)
 
 
 async def on_startup(dp: Dispatcher):
