2bfae5b69f27f6a46c00d9137f5ad7191d612736
shigaevaler@gmail.com <shigaevaler@gmail.com>
2022-04-06 19:09:57 +0400

    BQ registration module

--- db/db_service.py
+++ db/db_service.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-
 import pandas as pd
 from aiogram.types import Message
 from google.oauth2 import service_account
 
-from config import CREDENTIALS_PATH, PROJECT, cache
+from config import CREDENTIALS_PATH, PROJECT
 
 
 class DBManager:
@@ -26,49 +25,73 @@ class DBManager:
         и можно работать дальше
 
         :param message: сообщение пользователя
-        :type message: types.Message
+        :type message: Message
 
         :rtype: bool
         """
 
         tg_id = int(message.from_user.id)
         query = f"SELECT telegram_id FROM handy-digit-312214.TG_Bot_Stager.\
-                users WHERE telegram_id = {tg_id}"
+                users WHERE telegram_id = {tg_id} AND is_confirmed = true"
         if pd.read_gbq(query, project_id=self.project,
                        credentials=self.credentials).empty:
             return True
         return False
 
-    async def make_registration(self, message: Message) -> bool:
+    def registration(self, message: Message) -> None:
         """
-        Возвращает True, если запись успешно создана, создает запись
+        Записывает пользователя
 
-        :param message: сообщение пользователя
+        :param message: сообщение пользователя из которого ожидается email
         :type message: types.Message
-
-        :rtype: bool
         """
 
-        data = await cache.get_data(chat=message.chat,
-                                    user=message.from_user.username)
-        tg_id, tg_name = message.from_user.id, message.from_user.username
-        date = bytes(str(message.date.date()), "utf-8")
-        if self.check_user(message):
-            try:
-                df = pd.DataFrame({"telegram_id": [tg_id],
-                                   "telegram_name": [tg_name],
-                                   "email": [data["email"]],
-                                   "registration_code": [data["secret_key"]],
-                                   "is_confirmed": [True],
-                                   "regiestred_at": [date]})
-                df.to_gbq("handy-digit-312214.TG_Bot_Stager.users",
-                          project_id=self.project,
-                          credentials=self.credentials,
-                          if_exists="append")
-                return True
-            except (Exception,):
-                return False
-        return False
+        tg_id = message.from_user.id
+        tg_name = message.from_user.username
+        email = message.text
+        date = message.date
+        df = pd.DataFrame({"telegram_id": [tg_id],
+                           "telegram_name": [tg_name],
+                           "email": [email],
+                           "registration_code": [None],
+                           "is_confirmed": [False],
+                           "regiestred_at": [date]
+                           })
+        try:
+            df.to_gbq("handy-digit-312214.TG_Bot_Stager.users",
+                      project_id=self.project,
+                      credentials=self.credentials,
+                      if_exists="append")
+        except Exception as e:
+            print(e.args)
+
+    def authentication(self, message: Message):
+        """
+        Аутенцифицирует пользователя
+
+        :param message: сообщение пользователя из которого ожидается
+        secret key
+        :type message: types.Message
+        """
+
+        tg_id = message.from_user.id
+        secret_key = message.text
+        query = "SELECT * FROM handy-digit-312214.TG_Bot_Stager.users"
+        df = pd.read_gbq(query, project_id=self.project,
+                         credentials=self.credentials)
+        df2 = pd.DataFrame({'telegram_id': tg_id, 'is_confirmed': True,
+                            'registration_code': secret_key}, index=[0])
+        df.loc[df.telegram_id == tg_id, 'is_confirmed'] = \
+            df2[df2.telegram_id == tg_id].loc[0]['is_confirmed']
+        df.loc[df.telegram_id == tg_id, 'registration_code'] = \
+            df2[df2.telegram_id == tg_id].loc[0]['registration_code']
+        try:
+            df.to_gbq("handy-digit-312214.TG_Bot_Stager.users",
+                      project_id=self.project,
+                      credentials=self.credentials,
+                      if_exists="replace")
+        except Exception as e:
+            print(e.args)
 
     def send_task_to_bq(self,
                         user_id:
