5e82ff27cd0214003c72ac6e8cc8d2b33bb133e3
Anna Dovgal <a.dovgal@ylab.io>
2022-04-08 07:41:21 +0000

    Настроена работа шедулера с перезагрузкой бота

--- services/scheduler.py
+++ services/scheduler.py
@@ -1,20 +1,19 @@
 from datetime import date, datetime, timedelta
 
-from aiogram import Bot
-from aiogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from data import TIMEZONE
-from utils import db
+from data import executors, jobstores
 from loader import bot
-from services.is_day_off import is_day_off
-from services.graph import get_dataframe_for_graph, get_image, \
-    get_xlabel_for_graph
+from services import is_day_off
+from services.graph import (get_dataframe_for_graph, get_image,
+                            get_xlabel_for_graph)
+from utils import db
 
+SCHEDULER = AsyncIOScheduler(jobstores=jobstores, executors=executors)
 
-SCHEDULER = AsyncIOScheduler(timezone=TIMEZONE)
-db_manager = db.DBManager()
 TODAY = date.today()
+
+db_manager = db.DBManager()
 
 
 async def send_graph_to_all():
@@ -32,7 +31,7 @@ async def send_graph_to_all():
             labels = get_xlabel_for_graph(dataframe)
             get_image(dataframe, labels)
             await bot.send_photo(
-                       user_id, open("saved_graph.png", "rb"), caption=caption
+                user_id, open("saved_graph.png", "rb"), caption=caption
             )
 
 
@@ -51,11 +50,10 @@ def get_scheduler_for_payday():
 SCHEDULER.add_job(get_scheduler_for_payday, "cron", day="5,20", hour=10)
 
 
-async def send_reminder_to_user(bot: Bot, user_id: int, planned_at: datetime):
+async def send_reminder_to_user(user_id: int, planned_at: datetime):
     """
     Отправляет напоминание пользователю
 
-    :param bot: объект класса Bot
     :param user_id: id пользователя
     :param planned_at: дата события
     """
@@ -65,18 +63,16 @@ async def send_reminder_to_user(bot: Bot, user_id: int, planned_at: datetime):
 
 
 def set_scheduler(
-    message: Message,
-    user_id: int,
-    event: str,
-    event_date: str,
-    event_time: str,
-    comment: str,
+        user_id: int,
+        event: str,
+        event_date: str,
+        event_time: str,
+        comment: str,
 ):
     """
     Формирует задачу в планировщике и отдаёт напоминание на отправку
     пользователю в установленный срок
 
-    :param message: объект класса Message
     :param user_id: id пользователя
     :param event: название события
     :param event_date: дата события
@@ -98,10 +94,9 @@ def set_scheduler(
             event_date,
             created_at
     ):
-
         SCHEDULER.add_job(
             send_reminder_to_user,
             "date",
             run_date=reminder_time,
-            args=[message.bot, user_id, event_date],
+            args=[user_id, event_date],
         )
