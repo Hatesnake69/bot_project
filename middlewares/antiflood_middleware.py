ad9dc48d21fe1546d93404990fe28e9d65f53f95
Vladimir  Khrabrov <v.khrabrov@ylab.io>
2022-04-22 23:21:27 +0000

    Исправление ситуации, когда на заблокированного пользователя не срабатывала антифлуд мидлвара

--- middlewares/antiflood_middleware.py
+++ middlewares/antiflood_middleware.py
@@ -22,12 +22,8 @@ class AntiFloodThrottlingMiddleware(BaseMiddleware):
 
         :param message:
         """
-        # Get current handler
         handler = current_handler.get()
-
-        # Get dispatcher from context
         dispatcher = Dispatcher.get_current()
-        # If handler was configured, get rate limit and key from handler
         if handler:
             limit = getattr(handler, 'throttling_rate_limit', self.rate_limit)
             key = getattr(
@@ -38,15 +34,10 @@ class AntiFloodThrottlingMiddleware(BaseMiddleware):
         else:
             limit = self.rate_limit
             key = f"{self.prefix}_message"
-
-        # Use Dispatcher.throttle method.
         try:
             await dispatcher.throttle(key, rate=limit)
         except Throttled as t:
-            # Execute action
             await self.message_throttled(message, t)
-
-            # Cancel current handler
             raise CancelHandler()
 
     async def message_throttled(
@@ -71,22 +62,12 @@ class AntiFloodThrottlingMiddleware(BaseMiddleware):
             )
         else:
             key = f"{self.prefix}_message"
-
-        # Calculate how many time is left till the block ends
         delta = throttled.rate - throttled.delta
-
-        # Prevent flooding
         if throttled.exceeded_count <= 2:
             await message.reply(
                 'Слишком много запросов! Отправка сообщений ограничена.'
             )
-
-        # Sleep.
         await asyncio.sleep(delta)
-
-        # Check lock status
         thr = await dispatcher.check_key(key)
-
-        # If current message is not last with current key - do not send message
         if thr.exceeded_count == throttled.exceeded_count:
-            await message.reply('Unlocked.')
+            await message.reply('Разблокировано.')
